a053e7844e1dcd0fda999977b5c9b3a62605a9cb
Galih Saputro <iriantogalih1@gmail.com>
2024-05-22 11:35:36 +0700

    add code for create aws key pair and download private key in working directory

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
 provider "aws" {
   region     = "ap-southeast-1"  
 } 
+
+resource "aws_key_pair" "project1_key" {
+  key_name   = "aws-demo-key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "aws-demo-key.pem"
+}
